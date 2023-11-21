f52bd07dff919d8630e7ab2b3a00986d5ac9a8f0
Daniel Mendes <dm099150@gmail.com>
2023-11-21 10:50:38 -0300

    resolução de problema desvio1.c

--- desvio1.c
+++ desvio1.c
@@ -3,10 +3,11 @@ int main (){
     int nume ;
     printf("digite um número:\n");
     scanf("%d",&nume);
-    if(nume > 10);
+    if(nume > 10){
         printf("o número digitado é maior que 10 \n");
-    else
+    }
+    else {
        ("O numero digitado é menor ou igual a 10 \n ");
-
+    }
     return 0;
 }
