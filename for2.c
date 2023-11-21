7171c1007aca7386e9580f7d5c59ab8a18553ca1
Daniel Mendes <dm099150@gmail.com>
2023-11-21 11:58:35 -0300

    criação arquivo for1 e for2 com repetição

--- for2.c
+++ for2.c
@@ -4,10 +4,13 @@
 int main(){
     system("clear");
     int x;
+    int t = 0;
     for(x = 1 ; x <=100; x++){
         if(x % 2 == 0 ){
             printf("O Número %d é par\n",x);
+            t++;
         }
     }
+    printf("A quantidade de pares é %d\n",t);
     return 0;
 }
